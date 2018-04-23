a3ba1d75657732465dacf708a0e315b2bb044143
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Scheduler.CustomizationGotoDateForm/Controller/SchedulerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scheduler.CustomizationGotoDateForm.Models;
+using DevExpress.Web.Mvc;
+
+namespace Scheduler.CustomizationGotoDateForm {
+    public class SchedulerController: Controller {
+        public ActionResult Index() {
+            return RedirectToAction("Scheduler");
+        }
+        public ActionResult Scheduler() {
+            return View("Scheduler", SchedulerHelper.DataObject);
+        }
+        public ActionResult SchedulerPartial() {
+            return PartialView("SchedulerPartial", SchedulerHelper.DataObject);
+        }
+        public ActionResult EditAppointmentAction() {
+            UpdateDataObject();
+            return PartialView("SchedulerPartial", SchedulerHelper.DataObject);
+        }
+
+        void UpdateDataObject() {
+            InsertAppointment();
+            UpdateAppointments();
+            RemoveAppoinments();
+        }
+        void InsertAppointment() {
+            Schedule schedule = SchedulerExtension.GetAppointmentToInsert<Schedule>("scheduler", SchedulerHelper.DataObject.Appointments,
+                SchedulerHelper.DataObject.Resources, SchedulerHelper.AppointmentStorage, SchedulerHelper.ResourceStorage);
+            SchedulingDBDataProvider.InsertSchedule(schedule);
+        }
+        void UpdateAppointments() {
+            Schedule[] schedules = SchedulerExtension.GetAppointmentsToUpdate<Schedule>("scheduler", SchedulerHelper.DataObject.Appointments,
+                SchedulerHelper.DataObject.Resources, SchedulerHelper.AppointmentStorage, SchedulerHelper.ResourceStorage);
+            foreach (var schedule in schedules) {
+                SchedulingDBDataProvider.UpdateSchedule(schedule);
+            }
+        }
+        void RemoveAppoinments() {
+            Schedule[] schedules = SchedulerExtension.GetAppointmentsToRemove<Schedule>("scheduler", SchedulerHelper.DataObject.Appointments,
+                SchedulerHelper.DataObject.Resources, SchedulerHelper.AppointmentStorage, SchedulerHelper.ResourceStorage);
+            foreach (var schedule in schedules) {
+                SchedulingDBDataProvider.DeleteCarScheduling(schedule);
+            }
+        }
+    }
+}
---
+++ VB/Scheduler.CustomizationGotoDateForm/ExtensionHelpers/SchedulerExtensionHelper.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports DevExpress.Web.ASPxScheduler.Internal
+Imports DevExpress.Web.Mvc
+Imports DevExpress.XtraScheduler.Services
+
+Namespace Scheduler.CustomizationGotoDateForm.ExtensionHelpers
+	Public Class MVCxSchedulerGotoDateFormCallbackCommand
+		Inherits GotoDateFormCallbackCommand
+		Public Sub New(ByVal control As MVCxScheduler)
+			MyBase.New(control)
+		End Sub
+
+		Private privateNewDate As DateTime
+		Public Shadows Property NewDate() As DateTime
+			Get
+				Return privateNewDate
+			End Get
+			Protected Set(ByVal value As DateTime)
+				privateNewDate = value
+			End Set
+		End Property
+
+		Protected Overrides Sub ParseParameters(ByVal parameters As String)
+			Dim rawDate As String = HttpContext.Current.Request("NewDate")
+			NewDate = DateTime.Parse(rawDate)
+		End Sub
+		Protected Overrides Sub ExecuteCore()
+			Dim service As IDateTimeNavigationService = CType(Control.GetService(GetType(IDateTimeNavigationService)), IDateTimeNavigationService)
+			If service IsNot Nothing Then
+				service.GoToDate(NewDate)
+			End If
+		End Sub
+	End Class
+End Namespace
